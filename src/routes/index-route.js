eed7deff7e433dfcb33a67d873838b46caaf7b7b
eduardo-santos952 <duudu952@gmail.com>
2019-07-13 18:48:26 -0300

    CORS fix

--- src/routes/index-route.js
+++ src/routes/index-route.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
     res.status(200).send({
         title: "Node StarPlanet's API | Teste AME DIGITAL",
         version: "0.0.1"
